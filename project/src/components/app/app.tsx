eb06fe1aef924d5d9bfa561ec971386eaa447c22
Егор <Tiner69@yandex.ru>
2022-07-09 19:32:07 +0300

    Создал страницы и сгенерировал карточки фильмов

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
+import Home from '../../pages/home/Home';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <Home img={'img/the-grand-budapest-hotel-poster.jpg'}
+      alt={'The Grand Budapest Hotel poster'}
+      title={'The Grand Budapest Hotel'}
+      genre={'Drama'}
+      year={2014}
+    />);
 }
 
 export default App;
